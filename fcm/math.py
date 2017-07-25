d2f41e840015dda3c448d29fbb457f9bf1944f2a
wardence <warden.constance.e@gmail.com>
2017-07-25 14:29:01 -0400

    starts the fcm

---
+++ fcm/math.py
@@ -0,0 +1,23 @@
+"""
+A small set of functions for doing math operations.
+"""
+
+def add(a, b):
+    """
+    Function that adds 2 arguements
+    """
+    return a + b
+
+def mult(a, b):
+    """
+    Function that multiplies 2 arguements
+    """
+    return a * b
+
+def div(a, b):
+    """
+    Function that divides 2 arguements
+    """
+    a = float(a)
+    b = float(b)
+    return a / b
