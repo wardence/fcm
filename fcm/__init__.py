9bebab340161be129dc5b33ddaafc4ad2c8c1426
wardence <warden.constance.e@gmail.com>
2017-07-25 16:20:49 -0400

    added travis file from Daniel

--- fcm/__init__.py
+++ fcm/__init__.py
@@ -5,3 +5,4 @@ This is the base file of the friendly-computing-machine (fcm)!
 from . import math
 from .math import mult
 from .math import div
+from .math import add
--- fcm/math.py
+++ fcm/math.py
@@ -21,3 +21,28 @@ def div(a, b):
     a = float(a)
     b = float(b)
     return a / b
+
+def sub(a, b):
+    """
+    Function that subtracts 2 arguements
+    """
+    return a - b
+
+def rem(a, b):
+    """
+    Function that gives remainder of 2 arguements
+    """
+    return a % b
+
+def per(a):
+    """
+    Function that converts decimal to percent
+    """
+    return a * 100
+
+def dec(a):
+    """
+    Function that converts percent to decimal
+    """
+    a = float(a)
+    return a / 100
